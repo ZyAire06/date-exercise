f9d2eb88dd3c3258ad48b1f3527fb8039036f664
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-31 12:33:46 -0400

    add date_to_string.js

---
+++ date_to_string.js
@@ -0,0 +1,14 @@
+let d = new Date();
+console.log(d);
+
+// d.toString();
+console.log(d.toString());
+// d.toDateString();
+console.log(d.toDateString());
+// d.toUTCString();
+console.log( d.toUTCString());
+// d.toLocaleDateString();
+console.log(d.toLocaleDateString());
+
+
+
---
+++ offBy1Website/index.js
@@ -0,0 +1,19 @@
+window.onload = function (){
+    let todaysDate = document.getElementById("todaysDate");
+    todaysDate = addEventListener("click", todaysDateClicked)
+}
+
+
+function todaysDateClicked() {
+
+    // console.log("hey");
+    let pickADate = document.getElementById("pickADate").value;
+    console.log(pickADate);
+
+
+   
+    
+    
+}
+
+console.log(`${todaysDateClicked}`);
